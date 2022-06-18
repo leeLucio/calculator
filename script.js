d5905e2f8c0887269448276dde1c3054600c8357
Lucio <lucio.a00@gmail.com>
2022-06-18 18:14:46 -0300

    Add first math functions

---
+++ script.js
@@ -0,0 +1,36 @@
+function add(a,b){
+	return a+b;
+}
+
+function subtract(a,b){
+	return a-b;
+}
+
+function multiply(a,b){
+	return a*b;
+}
+
+function divide(a,b){
+	return a/b;
+}
+
+function operate(operator, a, b){
+	a = Number(a);
+	b = Number(b);
+	switch (operator) {
+		case "+":
+			return add(a,b);
+			
+		case "-":
+			return subtract(a,b);
+
+		case "x":
+			return multiply(a,b);
+
+		case "/":
+			return divide(a,b);
+
+		default:
+			break;
+	}
+}
