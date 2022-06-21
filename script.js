8abfc67f268872e1917c84294c17fd939dc0a2a5
Lucio <lucio.a00@gmail.com>
2022-06-21 20:28:53 -0300

    Add operations logic

--- script.js
+++ script.js
@@ -1,49 +1,81 @@
-function add(a,b){
-	return a+b;
+function add(a, b) {
+	return a + b;
 }
 
-function subtract(a,b){
-	return a-b;
+function subtract(a, b) {
+	return a - b;
 }
 
-function multiply(a,b){
-	return a*b;
+function multiply(a, b) {
+	return a * b;
 }
 
-function divide(a,b){
-	return a/b;
+function divide(a, b) {
+	return a / b;
 }
 
-function operate(operator, a, b){
+function operate(operator, a, b) {
 	a = Number(a);
 	b = Number(b);
+
 	switch (operator) {
 		case "+":
-			return add(a,b);
-			
+			return add(a, b);
+
 		case "-":
-			return subtract(a,b);
+			return subtract(a, b);
 
 		case "x":
-			return multiply(a,b);
+			return multiply(a, b);
 
 		case "/":
-			return divide(a,b);
+			return divide(a, b);
 
 		default:
 			break;
 	}
 }
 
+function clearDisplay() {
+	screen.textContent = "";
+}
+
 
 let numbers = document.querySelectorAll(".number");
-let screen = document.querySelector(".screen");
+let screen = document.querySelector(".display");
 let clear = document.querySelector(".clear");
+let operators = document.querySelectorAll(".operator");
+let screenResult = document.querySelector(".result");
 
-numbers.forEach(element => element.addEventListener("click", (event)=>{
+let pastNumber = 0;
+let pastOperator = null;
+let dotUsed = false;
+
+numbers.forEach(element => element.addEventListener("click", (event) => {
+	if(dotUsed && element.textContent == "."){
+		return;
+	}
+
 	screen.textContent = screen.textContent + event.target.textContent;
-}))
 
-clear.addEventListener("click", event =>{
-	screen.textContent = "";
+	if (element.textContent == ".") {
+		dotUsed = true;
+	}
+}));
+
+clear.addEventListener("click", clearDisplay);
+
+operators.forEach(element => {
+	element.addEventListener("click", () => {
+		if (pastOperator != null) {
+			result = operate(pastOperator, pastNumber, screen.textContent);
+			screen.textContent = result;
+		}
+
+		pastOperator = element.textContent;
+		pastNumber = Number(screen.textContent);
+		clearDisplay();
+		screenResult.textContent = pastNumber;
+		dotUsed = false;
+	})
 })
